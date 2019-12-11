2a69ea6ded56a5fd148d7b389ad4e30517441410
spitford <boyleb@hotmail.com>
2019-12-11 11:04:32 -0500

    Baseline Client
    Baseline commit for unity project

---
+++ Assets/Code/Networking/NetworkClient.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+using Project.Utility;
+
+namespace Project.Networking {
+    public class NetworkClient : SocketIOComponent {
+
+        [Header("Network Client")]
+        [SerializeField]
+        private Transform networkContainer;
+
+        private Dictionary<string, GameObject> serverObjects;
+        public override void Start() {
+            base.Start();
+            initialize();
+            setupEvents();
+        }
+
+        public override void Update() {
+            base.Update();
+        }
+
+        private void initialize() {
+            serverObjects = new Dictionary<string, GameObject>();
+        }
+
+        private void setupEvents() {
+            On("open", (E) => {
+                Debug.Log("Connected to Server");
+            });
+
+            On("register", (E) => {
+                string id = E.data["id"].ToString().RemoveQuotes();
+
+                Debug.LogFormat("Our Client's ID ({0})", id);
+            });
+
+            On("spawn", (E) => {
+                string id = E.data["id"].ToString().RemoveQuotes();
+
+                GameObject go = new GameObject("Server ID: " + id);
+                go.transform.SetParent(networkContainer);
+                serverObjects.Add(id, go);
+            });
+
+            On("disconnected", (E) => {
+                string id = E.data["id"].ToString().RemoveQuotes();
+
+                GameObject go = serverObjects[id];
+                Destroy(go);
+                serverObjects.Remove(id);
+            });
+        }
+    }
+}
