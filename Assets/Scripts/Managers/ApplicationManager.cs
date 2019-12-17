38bf9324da4cf30ca3cc05e4125bd799f129eda0
spitford <boyleb@hotmail.com>
2019-12-17 11:44:59 -0500

    Client side restructure of Menus

---
+++ Assets/Scripts/Managers/ApplicationManager.cs
@@ -0,0 +1,12 @@
+﻿using Project.Utility;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project.Managers {
+    public class ApplicationManager : MonoBehaviour {
+        void Start() {
+            SceneManagementManager.Instance.LoadLevel(SceneList.MAIN_MENU, (levelName) => {});
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,35 @@
+﻿using Project.Networking;
+using Project.Utility;
+using SocketIO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Project.Managers {
+    public class MenuManager : MonoBehaviour {
+
+        [SerializeField]
+        private Button queueButton;
+
+        private SocketIOComponent socketReference;
+
+        private SocketIOComponent SocketReference {
+            get {
+                return socketReference = (socketReference == null) ? FindObjectOfType<NetworkClient>() : socketReference;
+            }
+        }
+
+        void Start() {
+            queueButton.interactable = false;
+
+            SceneManagementManager.Instance.LoadLevel(SceneList.ONLINE, (levelName) => {
+                queueButton.interactable = true;
+            });
+        }
+
+        public void OnQueue () {
+            SocketReference.Emit("joinGame");
+        }
+    }
+}
